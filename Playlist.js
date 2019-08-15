ba6ecfd31e751b38462010185447c1bc76d359ec
shamb0t <shams.methnani@gmail.com>
2019-08-15 22:04:05 +0100

    play file

--- Playlist.js
+++ Playlist.js
@@ -3,8 +3,70 @@ import { observer, Observer } from 'mobx-react'
 import { Link } from 'react-router-dom'
 import DropZone from './DropZone.js'
 import ReactPlayer from 'react-player'
-import HlsjsIpfsLoader from 'hlsjs-ipfs-loader'
-import Hls from 'hls.js'
+
+function toArrayBuffer (buffer) {
+  const ab = new ArrayBuffer(buffer.length)
+  const view = new Uint8Array(ab)
+  for (let i = 0; i < buffer.length; ++i) {
+    view[i] = buffer[i]
+  }
+  return ab
+}
+
+const PlayAudio = ({ipfs, hash}) => {
+  const [previewContent, setPreviewContent] = useState(null)
+  useEffect (() => {
+    getFileBuffer(ipfs, hash).then(buffer => {
+      let blob = new Blob([])
+      if (buffer instanceof Blob) {
+        blob = buffer
+      } else if (buffer) {
+        blob = new Blob([toArrayBuffer(buffer)], { type: 'audio/mpeg' })
+      }
+      setPreviewContent(<audio src={window.URL.createObjectURL(blob)} controls autoPlay={false} />)
+    })
+  }, [hash])
+
+
+  // const srcUrl = buffer ? window.URL.createObjectURL(blob) : url
+  return previewContent ? previewContent : null
+}
+
+function getFileBuffer (ipfs, hash, options = {}) {
+  const timeoutError = new Error('Timeout while fetching file')
+  const timeout = options.timeout || 5 * 1000
+  return new Promise((resolve, reject) => {
+    let timeoutTimer = setTimeout(() => {
+      reject(timeoutError)
+    }, timeout)
+    const stream = ipfs.catReadableStream(hash)
+    let array = new Uint8Array(0)
+    stream.on('error', error => {
+      clearTimeout(timeoutTimer)
+      reject(error)
+    })
+    stream.on('data', chunk => {
+      clearTimeout(timeoutTimer)
+      const tmp = new Uint8Array(array.length + chunk.length)
+      tmp.set(array)
+      tmp.set(chunk, array.length)
+      array = tmp
+      timeoutTimer = setTimeout(() => {
+        reject(timeoutError)
+      }, timeout)
+    })
+    stream.on('end', () => {
+      console.log("FINITO")
+      clearTimeout(timeoutTimer)
+      resolve(array)
+    })
+  })
+}
+
+async function loadFile (hash) {
+  const array = await getFileBuffer(ipfs, hash)
+  return array
+}
 
 const Playlist = (props) => {
   const [playlist, setPlaylist] = useState(null)
@@ -28,52 +90,37 @@ const Playlist = (props) => {
     load()
 
     // reload when peer connected
-    props.store._ipfs.libp2p.on('peer:connect', load)
+    // props.store._ipfs.libp2p.on('peer:connect', load)
     return () => {
-      props.store._ipfs.libp2p.removeListener('peer:connect', load)
+      // props.store._ipfs.libp2p.removeListener('peer:connect', load)
       mounted = false
     }
   }
 
-    async function onDrop (event) {
-       event.preventDefault()
-       setDragActive(false)
-       const files = []
-       if (event.dataTransfer.items) {
-         for (let i = 0; i < event.dataTransfer.items.length; i++) {
-           const file = event.dataTransfer.items[i]
-           file.kind === 'file' && files.push(file.getAsFile())
-         }
-       } else {
-         for (let i = 0; i < event.dataTransfer.files.length; i++) {
-           files.push(event.dataTransfer.files.item(i))
-         }
+  async function onDrop (event) {
+     event.preventDefault()
+     setDragActive(false)
+     const files = []
+     if (event.dataTransfer.items) {
+       for (let i = 0; i < event.dataTransfer.items.length; i++) {
+         const file = event.dataTransfer.items[i]
+         file.kind === 'file' && files.push(file.getAsFile())
        }
-       try {
-         const newItems = await store.sendFiles(files, address)
-         // await playlist.load()
-         setItems(items.concat(newItems.filter(x => x!== null && x!== undefined)))
-       } catch (err) {
-         console.log("ERROR", err)
-         throw err
+     } else {
+       for (let i = 0; i < event.dataTransfer.files.length; i++) {
+         files.push(event.dataTransfer.files.item(i))
        }
-    }
-
-  async function play (testHash) {
-    Hls.DefaultConfig.loader = HlsjsIpfsLoader;
-    Hls.DefaultConfig.debug = false;
-    if (Hls.isSupported()) {
-      const video = document.getElementById('video');
-      const hls = new Hls();
-      hls.config.ipfs = props.store._ipfs;
-      hls.config.ipfsHash = testHash;
-      hls.loadSource('master.m3u8');
-      hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        video.play();
-      });
-    }
+     }
+     try {
+       const newItems = await store.sendFiles(files, address)
+       // await playlist.load()
+       setItems(items.concat(newItems.filter(x => x!== null && x!== undefined)))
+     } catch (err) {
+       console.log("ERROR", err)
+       throw err
+     }
   }
+
 
   return (
     <div onDragOver={event => {
@@ -85,7 +132,7 @@ const Playlist = (props) => {
       <ul> {
           items.map(song => (
             <div key={song.hash}>
-              <li>{song.payload.value.content}</li>
+              <li><PlayAudio ipfs={props.store._ipfs} hash={song.payload.value.content}/></li>
             </div>
           )
       )}
